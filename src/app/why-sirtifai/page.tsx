1637967361bbadc8c8b6b6893c5918b2273a0832
Ashwani Mishra <ashwani01superman@gmail.com>
2025-08-15 22:15:25 +0530

    why sirtifay

---
+++ src/app/why-sirtifai/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../../components/layout/Header"
+import { Footer } from "../../components/layout/Footer"
+
+export default function AboutPage() {
+  return (
+    <div className="font-sans min-h-screen overflow-x-hidden">
+      <Header />
+        why sirtifai
+      <Footer />
+    </div>
+  )
+}
